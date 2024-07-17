26e30ab2dad9e1c94099e2d928487817c7eb19bb
be <be.han.eps@gmail.com>
2024-07-17 16:00:46 -0700

    Update shape files

--- California Wildfire Seasonality/Be/fns.py
+++ California Wildfire Seasonality/Be/fns.py
@@ -24,5 +24,37 @@ def polycircle(x,y,r):
         coords.append((p.x, p.y))
     return shp.geometry.Polygon(coords)
 
+def polysquare(x,y,r,s):
+    return shp.geometry.box( (x-r)/s, (y-r)/s, (x+r)/s, (y+r)/s )
+
 def box_radius(arr):
     return haversine_distance(arr[0],arr[1],arr[2],arr[3])/2
+
+def roundby(x,n,r=0):
+    return (round(n*x+r)-r)/n
+
+def GPMindex(x1,x2):
+    return round(10*(x1-x2))
+
+def GPMcoord():
+    lon = [x/100 for x in range(-12445,-11405+5,10)]
+    lat = [y/100 for y in range(3245,4205+5,10)]
+    return [lon,lat]
+
+def CAbounds(i=None):
+    x1 = -124.482003000119
+    x2 = -114.131210999644
+    y1 = 32.5288320002052
+    y2 = 42.0095030001829
+    if i == None:
+        return [x1,y1,x2,y2]
+    elif i == 0:
+        return [int(roundby(x1-10**(-i)/2,10**i)),
+                int(roundby(y1-10**(-i)/2,10**i)),
+                int(roundby(x2+10**(-i)/2,10**i)),
+                int(roundby(y2+10**(-i)/2,10**i))]
+    else:
+        return [roundby(x1-10**(-i)/2,10**i),
+                roundby(y1-10**(-i)/2,10**i),
+                roundby(x2+10**(-i)/2,10**i),
+                roundby(y2+10**(-i)/2,10**i)]
